98fd33202df57c12954c77827ac742cf38cc7710
unknown <kozhin.svetoslav68@gmail.com>
2025-08-19 01:22:04 +1200

    быстро набросил примерно, Как может выглядеть, но, города и данных перменных У[]П[] и т.д. у нас здесь нет, также придется перерабатывать апи, также не понятно, Как именно они должны отрисосываться

--- src/components/AllClasses.jsx
+++ src/components/AllClasses.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Row, Col, Card, Spinner } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import OkBadgeDate from "./custom/OkBadgeDate";
+import OkBadgeDate from "./custom/OkBadgeDateCrm";
+// import OkBadgeDate from "./custom/OkBadgeDate";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
@@ -71,7 +72,7 @@ function AllClasses() {
                 <div className="d-flex justify-content-between">
                   <Card.Title className="mb-2">
                     <div className="d-flex align-items-center">
-                      <span style={{ fontSize: "14px" }}>
+                      <span style={{ fontSize: "25px" }}>
                         <Link to={`/classes/single_class/${item.Id}`}>
                           {item.ClientName}
                         </Link>
@@ -79,31 +80,11 @@ function AllClasses() {
                     </div>
                   </Card.Title>
                 </div>
-                <div className="mb-2 text-body-secondary">
-                  <Row>
-                    <Col sm={1} className="pt-1" style={{ fontSize: "12px" }}>
-                      <i className="bi bi-telephone "></i>
-                    </Col>
-                    <Col sm={10}>
-                      <small style={{ fontSize: "11px" }}>
-                        8-888-888-88-88
-                      </small>
-                    </Col>
-                  </Row>
+                <div className="d-flex justify-content-between">
+                  <small style={{ fontSize: "25px" }}>Петропавловск Камчатский{/*Мы здесь не получаем город, нужно менять апи*/}</small>
+                  <small style={{ fontSize: "25px" }}>У[25]П[5]Ц[3]А[7]О[2]Ф[J]</small>
                 </div>
-                <div
-                  className="mb-2 text-body-secondary"
-                  style={{ fontSize: "12px" }}
-                >
-                  <Row>
-                    <Col sm={1} className="pt-1">
-                      <i className="bi bi-chat-right-text me-3"></i>
-                    </Col>
-                    <Col sm={10}>
-                      <small>Записка менеджера</small>
-                    </Col>
-                  </Row>
-                </div>
+
 
                 <OkBadgeDate date={"2025-04-20"} />
               </Card.Body>
@@ -116,3 +97,45 @@ function AllClasses() {
 }
 
 export default AllClasses;
+
+//БЫЛО           <Card.Body style={{ paddingBottom: "10px" }}>
+//                 <div className="d-flex justify-content-between">
+//                   <Card.Title className="mb-2">
+//                     <div className="d-flex align-items-center">
+//                       <span style={{ fontSize: "14px" }}>
+//                         <Link to={`/classes/single_class/${item.Id}`}>
+//                           {item.ClientName}
+//                         </Link>
+//                       </span>
+//                     </div>
+//                   </Card.Title>
+//                 </div>
+//                 <div className="mb-2 text-body-secondary">
+//                   <Row>
+//                     <Col sm={1} className="pt-1" style={{ fontSize: "12px" }}>
+//                       <i className="bi bi-telephone "></i>
+//                     </Col>
+//                     <Col sm={10}>
+//                       <small style={{ fontSize: "11px" }}>
+//                         8-888-888-88-88
+//                       </small>
+//                     </Col>
+//                   </Row>
+//                 </div>
+//                 <div
+//                   className="mb-2 text-body-secondary"
+//                   style={{ fontSize: "12px" }}
+//                 >
+//                   <Row>
+//                     <Col sm={1} className="pt-1">
+//                       <i className="bi bi-chat-right-text me-3"></i>
+//                     </Col>
+//                     <Col sm={10}>
+//                       <small>Записка менеджера</small>
+//                     </Col>
+//                   </Row>
+//                 </div>
+
+//                 <OkBadgeDate date={"2025-04-20"} />
+//               </Card.Body>
+//  
